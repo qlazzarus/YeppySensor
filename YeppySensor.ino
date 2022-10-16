f3f2a731d4698252f2da8373f944f467fe7ccfa1
monoless <ecst@monolife.co.kr>
2022-10-16 22:38:43 +0900

    moving average filter

--- YeppySensor.ino
+++ YeppySensor.ino
@@ -18,10 +18,7 @@ byte threeImage[] = {0x18, 0x04, 0x18, 0x04, 0x18, 0x00, 0x00, 0x00};
 // digital pin
 const int DHT_PIN = 2;
 const int DUST_PIN = 8;
-const int BUZZER_PIN = 5;
-const int MOTOR_PIN = 6;
-
-const int COUNT_NUM = 10;
+const int BUZZER_PIN = 5; // relay will be here
 
 // 온습도 센서
 float humidity = 0;
@@ -29,16 +26,20 @@ float temperature = 0;
 
 // dust sensor
 unsigned long duration;
-unsigned long mainCalcTime;
-// 먼지센서 샘플링 시간 - 5sec
-unsigned long sampleTime = 5000;
+unsigned long calcTime;
+unsigned long sampleTime = 2000;
 unsigned long lowPulseOccupancy = 0;
 float ratio = 0;
 float concentration = 0;
 float dustDensity = 0;
-boolean mainCheck = false;
-unsigned int dustState = 0;
-unsigned int buzzerCount = 0;
+
+// sample moving average filter
+const int COUNT_NUM = 10;
+float dustArray[COUNT_NUM];
+int readIndex = 0;
+float dustTotal = 0;
+float dustAverage = 0;
+
 
 // initial DHT11
 DHT11 dht11(DHT_PIN);
@@ -59,6 +60,7 @@ void initialPin() {
 void initialLcd() {
   lcd.begin();
   lcd.backlight();
+  //lcd.noBacklight();
 
   lcd.createChar(0, humidityImage);
   lcd.createChar(1, temperatureImage);
@@ -70,38 +72,37 @@ void initialLcd() {
   lcd.print("EJLOVE!!");
 }
 
+void initialAverage() {
+  for (int i = 0; i < COUNT_NUM; i++) {
+    dustArray[i] = 0;
+  }
+}
 
-/**
-   신뢰할 수 있는 먼지밀도 계산하기
-   대부분의 아날로그센서의 경우 값이 튀는 현상이 있는데,
-   이것을 보정하기 위해 여러번 값을 누적한 후,
-   평균값을 내어 신뢰할 수 있는 먼지밀도를 구합니다.
-*/
+
 void calcSensor() {
   duration = pulseIn(DUST_PIN, LOW);
   lowPulseOccupancy += duration;
 
-  // 먼지센서 샘플링 시간 - 2sec 동안 duration / lowPulseOccupancy 로 확인
-  if ((millis() - mainCalcTime) > sampleTime) {
+  if ((millis() - calcTime) > sampleTime) {
     ratio = lowPulseOccupancy / (sampleTime * 10.0);
     // using spec sheet curve
     concentration = 1.1 * pow(ratio, 3) - 3.8 * pow(ratio, 2) + 520 * ratio + 0.62;
 
     dustDensity = (concentration * 100) / 13000;
-    lowPulseOccupancy = 0.0;
 
-    /*
-    dustState = 0;
-    if (dustDensity > 150) {
-      dustState = 3;
-    } else if (dustDensity > 80) {
-      dustState = 2;
-    } else if (dustDensity > 30) {
-      dustState = 1;
+    dustTotal -= dustArray[readIndex];
+    dustArray[readIndex] = dustDensity;
+    dustTotal += dustArray[readIndex];
+    readIndex++;
+
+    if (readIndex >= COUNT_NUM) {
+      readIndex = 0;
     }
-    */
 
-    mainCalcTime = millis();
+    dustAverage = dustTotal / COUNT_NUM;
+    
+    lowPulseOccupancy = 0.0;
+    calcTime = millis();
     dht11.read(humidity, temperature);
   }
 }
@@ -111,11 +112,11 @@ void calcSensor() {
 void printLcd() {
   //lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(dustDensity);
+  lcd.print(dustAverage);
   lcd.write(3);
   lcd.print("g/m");
   lcd.write(4);
-  lcd.setCursor(10, 0);
+  lcd.print("  ");
 
   lcd.setCursor(0, 1);
   lcd.write(0);
@@ -139,11 +140,11 @@ void setup() {
   initialSerial();
   initialPin();
   initialLcd();
-  mainCalcTime = millis();
+  initialAverage();
+  calcTime = millis();
 }
 
 void loop() {
-  // 부저 + 팬 + 
   calcSensor();
   printLcd();
 }
